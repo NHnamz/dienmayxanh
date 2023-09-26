010f91f9cb0d6ca7e94797a2d81227690ab78f14
NHnamz <93305227+NHnamz@users.noreply.github.com>
2023-09-26 17:22:30 +0700

    first commit

---
+++ build/js/index.js
@@ -0,0 +1,48 @@
+const swiper_left = new Swiper('.swiper_left', {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  grabCursor: true,
+});
+
+const swiper_rifht = new Swiper('.swiper_right', {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  grabCursor: true,
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 2,
+    }
+  }
+});
+
+const show = () => {
+  document.querySelector(".more").classList.add("hidden")
+  document.querySelector(".list").classList.remove("hidden")
+}
+
+const showContent = () => {
+  document.querySelector(".list_content").classList.remove("hidden")
+}
